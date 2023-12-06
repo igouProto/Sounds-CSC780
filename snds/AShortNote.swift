89391507cb3cdf196de6c230ef74ccfe41b479ba
igouProto <48474345+igouProto@users.noreply.github.com>
2023-12-05 17:36:01 -0800

    just a short note

---
+++ snds/AShortNote.swift
@@ -0,0 +1,8 @@
+//
+//  AShortNote.swift
+//  snds
+//
+//  Created by Reina Kawamoto on 2023/12/5.
+//
+//  In case you're wondering who "Reina Kawamoto" is
+//  This is the username of my Mac's user account
